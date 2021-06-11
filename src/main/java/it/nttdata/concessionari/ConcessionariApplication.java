4208530d2ce05577bc4626999dc921da79c0b787
Luca Sorrentino <sorrentinoluca.1999@gmail.com>
2021-06-11 11:28:44 +0200

    Initial commit

---
+++ src/main/java/it/nttdata/concessionari/ConcessionariApplication.java
@@ -0,0 +1,13 @@
+package it.nttdata.concessionari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcessionariApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcessionariApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/nttdata/concessionari/ConcessionariApplicationTests.java
@@ -0,0 +1,13 @@
+package it.nttdata.concessionari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcessionariApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
